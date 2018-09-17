6f7860e89af197a681f070732d76a2a89924fb05
Emil Tabakov <emil.tabakov@gmail.com>
2018-09-17 18:23:24 +0300

    feat: add tracking of custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,16 +20,7 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this._activatedUrl = "/home";
-        this._sideDrawerTransition = new SlideInOnTopTransition();
-
-        this.router.events
-        .pipe(filter((event: any) => event instanceof NavigationEnd))
-        .subscribe((event: NavigationEnd) => this._activatedUrl = event.urlAfterRedirects);
-
         firebase.init({
-            // Optionally pass in properties for database, authentication and cloud messaging,
-            // see their respective docs.
           }).then(
             instance => {
               console.log("firebase.init done");
@@ -38,6 +29,29 @@ export class AppComponent implements OnInit {
               console.log(`firebase.init error: ${error}`);
             }
           );
+
+        this._activatedUrl = "/home";
+        this._sideDrawerTransition = new SlideInOnTopTransition();
+
+        this.router.events
+        .pipe(filter((event: any) => event instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+            this._activatedUrl = event.urlAfterRedirects;
+            console.log("navigate");
+            firebase.analytics.logEvent({
+                key: "page_view",
+                parameters: [
+                    {
+                        key: "page_id",
+                        value: this._activatedUrl
+                    }
+                ]
+            });
+
+            firebase.analytics.setScreenName({
+                screenName: this._activatedUrl
+            });
+        });
     }
 
     get sideDrawerTransition(): DrawerTransitionBase {
